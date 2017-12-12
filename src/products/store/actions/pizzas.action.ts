d9d179ed45a823a1c513f7b2c49ac6e7ab92bf54
daliyajohn <daliyajohn75@gmail.com>
2017-12-12 13:00:48 +0530

    Integrate action and logics

--- src/products/store/actions/pizzas.action.ts
+++ src/products/store/actions/pizzas.action.ts
@@ -1,6 +1,23 @@
 import { Action } from '@ngrx/store';
+import { Pizza } from 'src/products/models/pizza.model';
 
 // load pizzas
 export const LOAD_PIZZAS = '[Products] load pizzas';
 export const LOAD_PIZZAS_FAIL = '[Products] load pizzas fail';
 export const LOAD_PIZZAS_SUCCESS = '[Products] load pizzas success';
+
+export class LoadPizzas implements Action {
+    readonly type: 'LOAD_PIZZAS';
+}
+
+export class LoadPizzasFail implements Action {
+    readonly type: 'LOAD_PIZZAS_FAIL';
+    constructor(public payload: any) {}
+}
+
+export class LoadPizzasSuccess implements Action {
+    readonly type: 'LOAD_PIZZAS_SUCCESS';
+    constructor(public payload: Pizza[]) {}
+}
+
+export type PizzasAction = LoadPizzas | LoadPizzasFail | LoadPizzasSuccess;
