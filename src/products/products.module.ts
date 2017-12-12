25846b35af56ac17ed64831ef32ddaa0b672a28e
daliyajohn <daliyajohn75@gmail.com>
2017-12-12 17:01:30 +0530

    fix

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
+import { reducerConnect } from './store';
 
 // components
 import * as fromComponents from './components';
@@ -36,7 +37,7 @@ export const ROUTES: Routes = [
     ReactiveFormsModule,
     HttpClientModule,
     RouterModule.forChild(ROUTES),
-    StoreModule.forFeature()
+    StoreModule.forFeature('products',reducerConnect )
   ],
   providers: [...fromServices.services],
   declarations: [...fromContainers.containers, ...fromComponents.components],
