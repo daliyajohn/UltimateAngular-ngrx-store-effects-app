9a8e8da3e1c16c1050d23b15110c645a2d7b1899
daliyajohn <daliyajohn75@gmail.com>
2017-12-12 10:37:40 +0530

    Add structure

---
+++ src/products/store/actions/pizzas.action.ts
@@ -0,0 +1,6 @@
+import { Action } from '@ngrx/store';
+
+// load pizzas
+export const LOAD_PIZZAS = '[Products] load pizzas';
+export const LOAD_PIZZAS_FAIL = '[Products] load pizzas fail';
+export const LOAD_PIZZAS_SUCCESS = '[Products] load pizzas success';
